6cb12cb454e7ce7f92a904cd67f065b90a794b06
Eren KÜRPE <84393333+kurpeeren@users.noreply.github.com>
2024-04-23 16:16:01 +0300

    Add project files.

---
+++ auto-wifi-reset/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace auto_wifi_reset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Auto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Standby mode active.";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button2.Location = new System.Drawing.Point(12, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Enable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button3.Location = new System.Drawing.Point(12, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Disable";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Default Gateway:";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(122, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(40, 40);
+            this.panel1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Target Gateway:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(280, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(188, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Get Target Gateway";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::auto_wifi_reset.Properties.Resources.siyah_AEK;
+            this.pictureBox1.Location = new System.Drawing.Point(240, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 183);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(412, 222);
+            this.MinimumSize = new System.Drawing.Size(412, 222);
+            this.Name = "Form1";
+            this.Text = "wifi-reset";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ auto-wifi-reset/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Diagnostics;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace auto_wifi_reset
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        Thread thread1;
+        public Form1()
+        {
+            InitializeComponent();
+            thread1 = new Thread(taskloop);
+            CheckForIllegalCrossThreadCalls =false;
+            if (!WifiManager.IsUserAdministrator())
+            {
+                label1.Text = "Program is not running as administrator.";
+            }
+        }
+
+        [Obsolete]
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(thread1.ThreadState == System.Threading.ThreadState.Running)
+            { thread1.Abort(); }
+            else {
+                thread1 = new Thread(taskloop); 
+                thread1.Start(); }
+
+
+
+        }
+        bool tick=true;
+        Thread thread2;
+        public void taskloop()
+        {
+            while (true)
+            {
+
+                textBox1.Text = WifiManager.GetDefaultGateway();
+                if (textBox2.Text != textBox1.Text)
+                {
+                    WifiManager.DisableWifi();
+                    while (WifiManager.IsWifiEnabled())
+                    { Thread.Sleep(1000);
+                        panel1.BackColor = tick ? Color.LightGreen : Color.Orange;
+                        tick = !tick;
+                    }
+                }
+
+
+                bool wifiEnabled = WifiManager.IsWifiEnabled();
+
+                if (wifiEnabled)
+                {
+                    label1.Text = "WiFi is still enabled.";
+                }
+                else
+                {
+                    label1.Text = "WiFi is disabled.";
+                    WifiManager.EnableWifi();
+                    while(!WifiManager.IsWifiEnabled())
+                    {
+                        Thread.Sleep(1000);
+                        panel1.BackColor = tick ? Color.LightGreen : Color.Orange;
+                        tick = !tick;
+                    }
+                    textBox1.Text = WifiManager.GetDefaultGateway();
+                }
+                panel1.BackColor = tick ? Color.LightGreen : Color.Green;
+                tick = !tick;
+                Thread.Sleep(1000);
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WifiManager.EnableWifi();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WifiManager.DisableWifi();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = WifiManager.GetDefaultGateway();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (thread1.ThreadState != System.Threading.ThreadState.Aborted)
+            { thread1.Abort(); }
+        }
+
+    }
+
+
+
+    public class WifiManager
+    {
+        public static bool IsUserAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public static string GetDefaultGateway()
+        {
+            string defaultGateway = null;
+
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (GatewayIPAddressInformation gateway in ni.GetIPProperties().GatewayAddresses)
+                    {
+                        if (gateway.Address != null)
+                        {
+                            defaultGateway = gateway.Address.ToString();
+                            break;
+                        }
+                    }
+                }
+                if (defaultGateway != null)
+                    break;
+            }
+
+            return defaultGateway;
+        }
+
+        private static void ExecuteCommand(string command)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe")
+            {
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process p = Process.Start(psi);
+            if (p != null)
+            {
+                p.StandardInput.WriteLine(command);
+                p.StandardInput.Flush();
+                p.StandardInput.Close();
+                p.WaitForExit();
+            }
+        }
+
+        public static void DisableWifi()
+        {
+            ExecuteCommand("netsh interface set interface Wi-Fi admin=disable");
+        }
+
+        public static void EnableWifi()
+        {
+            ExecuteCommand("netsh interface set interface Wi-Fi admin=enable");
+        }
+
+        public static bool IsWifiEnabled()
+        {
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionId='Wi-Fi'");
+            ManagementObjectCollection adapters = searcher.Get();
+
+            foreach (ManagementObject adapter in adapters)
+            {
+                bool enabled = (bool)adapter["NetEnabled"];
+                return enabled;
+            }
+
+            return false;
+        }
+
+    }
+
+
+
+
+    
+}
